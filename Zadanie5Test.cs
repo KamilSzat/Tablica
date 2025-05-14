a04332f97e03bf2a87b3dae514c5bcc6ad034ab2
KamilSzat <uT_3776@zsp3zamosc.pl>
2025-05-14 08:25:06 +0200

    Zadanie5 z tablicą

---
+++ Zadanie5Test.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie5
+{
+    [TestClass]
+    public class Zadanie5Test
+    {
+        [TestMethod]
+
+        public void Reverse_Array_ReturnReversed()
+        {
+            var z = new Zadanie5();
+            var input = new[] { 1, 2, 3 };
+            var result = z.Reverse(input);
+
+            CollectionAssert.AreEqual(new[] { 3, 2, 1 }, result);
+        }
+    }
+}
